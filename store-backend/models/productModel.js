75a61111e5f9e1f67ea1944dcb3794409d9a0801
Excel <000.excel@gmail.com>
2020-09-27 10:26:17 +0530

    validation applied

--- store-backend/models/productModel.js
+++ store-backend/models/productModel.js
@@ -1,14 +1,49 @@
 const mongoose = require("mongoose");
+const { isURL, isAlphanumeric, isAlpha, isFloat } = require("validator");
 const productSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, "Product name is required"],
+    validate: [/^[0-9a-z\- ]+$/, "Provide a correct Name"],
+  },
   model: String,
-  image: String,
-  description: String,
-  price: Number,
-  rating: Number,
+  image: {
+    type: String,
+    required: [true, "Product image is required"],
+    validate: [isURL, "Provide a correct URL"],
+  },
+  description: {
+    type: String,
+    validate: [
+      /^[0-9a-z\-, ]+$/,
+      "Provide a correct description special char allowed(,)",
+    ],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price required"],
+    min: [0, "price must be greater 0"],
+    set: (val) => Math.round(val * 100) / 100,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+    validate: {
+      validator: (data) => {
+        console.log(data);
+        return data <= 5 && data >= 0;
+      },
+      message: (props) => `${props.value} is not a valid rating`,
+    },
+  },
   quantity: Number,
   category: String,
-  discount: Number,
+  discount: {
+    type: Number,
+    default: 0,
+    min: [0, "discount must be greater 0"],
+    max: [100, "discount must be greater 100"],
+  },
 });
 const ProductModel = mongoose.model("products", productSchema);
 
