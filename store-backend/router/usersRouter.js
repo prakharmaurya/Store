066df9ab0ae46a74c611504234074ce7cb796da1
Excel <000.excel@gmail.com>
2020-09-29 11:15:40 +0530

    login token checker route applied

--- store-backend/router/usersRouter.js
+++ store-backend/router/usersRouter.js
@@ -2,15 +2,24 @@ const router = require("express").Router();
 const {
   getAllUsers,
   getAUser,
-  signUp,
-  login,
   updateAUser,
   deleteAUser,
 } = require("../controllers/usersController");
 
+const {
+  signUp,
+  login,
+  updateMe,
+  tokenChecker,
+} = require("../controllers/authController");
+
 // router
 router.route("/login").post(login);
-router.route("/").get(getAllUsers).post(signUp);
+router.route("/signup").post(signUp);
+
+router.route("/updateMe").patch(tokenChecker, updateMe);
+
+router.route("/").get(getAllUsers);
 
 router.route("/:id").get(getAUser).patch(updateAUser).delete(deleteAUser);
 
