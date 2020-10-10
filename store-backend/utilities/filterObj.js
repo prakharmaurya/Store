d0e9334f4571016dce4ce1415421b2e3bb199f1d
Excel <000.excel@gmail.com>
2020-10-10 09:38:24 +0530

    object filter 2nd type implemnted

--- store-backend/utilities/filterObj.js
+++ store-backend/utilities/filterObj.js
@@ -1,18 +1,24 @@
 exports.rejectFilter = (arr, obj) => {
-  const newObj = {};
+  // const newObj = {};
+
+  // Object.keys(obj).forEach((key) => {
+  //   let flag = false;
+  //   arr.forEach((k) => {
+  //     if (k === key) flag = true;
+  //   });
+
+  //   if (!flag) {
+  //     newObj[key] = obj[key];
+  //   }
+  // });
 
   Object.keys(obj).forEach((key) => {
-    let flag = false;
-    arr.forEach((k) => {
-      if (k === key) flag = true;
-    });
-
-    if (!flag) {
-      newObj[key] = obj[key];
+    if (arr.includes(key)) {
+      delete obj[key];
     }
   });
 
-  return newObj;
+  return obj;
 };
 
 exports.acceptFilter = (arr, obj) => {
